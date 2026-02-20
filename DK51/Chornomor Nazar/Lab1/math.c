0ebcecaf1be008520d0ed97e6f7b9904da6245ea
MrNNCC <92216407+MrNNCC@users.noreply.github.com>
2026-02-20 02:50:17 +0200

    Add files via upload

--- DK51/Chornomor Nazar/Lab1/math.c
+++ DK51/Chornomor Nazar/Lab1/math.c
@@ -1,13 +1,14 @@
+#include "math.h"
+
 int absolute(int a) {
-    if (a <= 0) {
-        return -a;
-    } else {
-        return a;
-    }
+    return a < 0 ? -a : a;
 }
 
 int factorial(int a) {
+    if (a < 0) return -1;
+    
     int result = 1;
+
     for (int i = 1; i <= a; i++)
         result *= i;
     return result;
