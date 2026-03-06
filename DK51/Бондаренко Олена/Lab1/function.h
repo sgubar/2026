6486355d4c77e0e2aa81906060c2d76e392d0b54
OlenaBondarenko578 <elena.bondarenko0508@gmail.com>
2026-03-06 15:25:24 +0200

    Add function.h with mathematical function declarations

---
+++ DK51/Бондаренко Олена/Lab1/function.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+double absolute_value(double x);
+double power(double base, int exponent);
+double sum_2D(int A);
+double q(int A, double B, double C);
+
+#endif
