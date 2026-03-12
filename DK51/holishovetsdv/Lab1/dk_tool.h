3ac170e62f0aaee59a51a30550496bf4c63ea91c
holishovetsdv-dk51-design <holishovets.d.v.-dk51@lll.kpi.ua>
2026-03-12 23:56:25 +0200

    Add files via upload

---
+++ DK51/holishovetsdv/Lab1/dk_tool.h
@@ -0,0 +1,11 @@
+
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
+
+double get_double_input(const char *prompt);
+int get_int_input(const char *prompt);
+
+int factorial(int base);
+double calculate_expression(double A, int B, double C);
+
+#endif
