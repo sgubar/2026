35a7ffe53a82cf5e754dc70e8a8058cb3874e451
w1nsston <lordcanell@gmail.com>
2026-03-21 16:32:09 +0200

    hw1

---
+++ DK51/Artyom Lytvyn/HW1/dk_tool.h
@@ -0,0 +1,11 @@
+
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
+
+
+int count_substring(const char* sentence, const char* word);
+
+
+void read_string(char* buffer, int max_length, const char* prompt);
+
+#endif //
