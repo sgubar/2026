6f837ac587fd36a39fa4c6ec9f2604ced11871ec
Wandyyy88 <79667795+WANDYYY0@users.noreply.github.com>
2026-04-10 15:24:54 +0300

    Add files via upload

--- DK51/Kolomoetc Georgiy/HW5/main.c
+++ DK51/Kolomoetc Georgiy/HW5/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include "tree.h"
 
+int main() {
+    BinaryTree* my_tree = create_tree();
+
+    insert_element(my_tree, 50.55);
+    insert_element(my_tree, 30.22);
+    insert_element(my_tree, 70.88);
+    insert_element(my_tree, 20.11);
+    insert_element(my_tree, 40.44);
+    insert_element(my_tree, 60.66);
+    insert_element(my_tree, 80.99);
+
+    print_tree(my_tree);
+
+    double search_val = 40.44;
+    Node* found = search_element(my_tree, search_val);
+    if (found != NULL) {
+        printf("Found element: %.2f\n\n", found->data);
+    } else {
+        printf("Element %.2f not found\n\n", search_val);
+    }
+
+    printf("Deleting element 30.22...\n");
+    delete_element(my_tree, 30.22);
+    print_tree(my_tree);
+
+    delete_tree(my_tree);
+
+    return 0;
+}
---
+++ DK51/Kolomoetc Georgiy/HW5/tree.h
@@ -0,0 +1,21 @@
+#ifndef TREE_H
+#define TREE_H
+
+typedef struct Node {
+    double data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+typedef struct {
+    Node* root;
+} BinaryTree;
+
+BinaryTree* create_tree();
+void insert_element(BinaryTree* tree, double value);
+void delete_element(BinaryTree* tree, double value);
+Node* search_element(BinaryTree* tree, double value);
+void delete_tree(BinaryTree* tree);
+void print_tree(BinaryTree* tree);
+
+#endif
