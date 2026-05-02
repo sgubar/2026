c4dc553b3571d8adf2d13cf57fa4aa7cbfa496c7
OlenaBondarenko578 <elena.bondarenko0508@gmail.com>
2026-05-03 00:11:30 +0300

    Add point.h header file for Point structure

---
+++ DK51/Бондаренко Олена/Lab4/point.h
@@ -0,0 +1,19 @@
+#ifndef point_h
+#define point_h
+
+#include <stdio.h>
+
+struct tagPoint {
+    int x;
+    int y;
+};
+typedef struct tagPoint Point;
+
+Point *createPoint(int x, int y);
+Point *copyPoint(Point *a);
+void deletePoint(Point *a);
+void printPoint(Point *a);
+void fillPoint(Point *a);
+void setPointValues(Point* point, int x, int y);
+
+#endif /* point_h */
